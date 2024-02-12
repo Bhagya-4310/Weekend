d5b454ff2e7fff9a39739f652b5f3db01b1099ee
Bhagya-4310 <157447593+Bhagya-4310@users.noreply.github.com>
2024-02-12 11:55:15 +0530

    working days added

---
+++ Workingdays.java
@@ -0,0 +1,6 @@
+public class Workingdays {
+    public String noofdays;
+    public Workingdays(){
+        noofdays="five";
+    }
+}
--- src/wokingdays.java
+++
@@ -1,3 +0,0 @@
-public class wokingdays {
-    public String week1;
-}
